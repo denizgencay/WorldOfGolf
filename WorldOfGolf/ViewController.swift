4461d1b788a627ec9b8096f0c871e235ce946892
denizgencay <denizgencay35@gmail.com>
2022-07-13 18:00:20 +0300

    project init

---
+++ WorldOfGolf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldOfGolf
+//
+//  Created by Deniz Eren Gençay on 13.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
