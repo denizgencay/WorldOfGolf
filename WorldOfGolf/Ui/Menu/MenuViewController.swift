46279b8b19e4bfe2ad7a2fc325889a6a44a249dd
Cem <cemsertkaya99@gmail.com>
2022-07-16 15:32:49 +0300

    Menu and segues are completed

--- WorldOfGolf/Ui/Menu/MenuViewController.swift
+++ WorldOfGolf/Ui/Menu/MenuViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController
+{
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -16,23 +17,30 @@ class MenuViewController: UIViewController {
         tableView.dataSource = self
     }
     
-    @objc func buttonTapped(_ sender: UIButton) {
-        switch sender.tag{
-        case 1:
-            print("")
-        default:
-            print("Error")
+    @objc func buttonTapped(_ sender: UIButton)
+    {
+        switch sender.tag
+        {
+            case 1: self.performSegue(withIdentifier: "toMain", sender: self)
+            case 2: self.performSegue(withIdentifier: "toRules", sender: self)
+            case 3: self.performSegue(withIdentifier: "toDetails", sender: self)
+            case 4: self.performSegue(withIdentifier: "toInventory", sender: self)
+            case 5: self.performSegue(withIdentifier: "toChampionships", sender: self)
+            case 6: self.performSegue(withIdentifier: "toAthletes", sender: self)
+            default:print("Error")
         }
     }
 
 }
 
-extension MenuViewController: UITableViewDelegate, UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 7
-    }
+extension MenuViewController: UITableViewDelegate, UITableViewDataSource
+{
     
-    func guardTableView(title: String ,indexPath: IndexPath) -> MenuSecondTableViewCell{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {return 7}
+    
+    func initCell(title: String ,indexPath: IndexPath, imagePath : String) -> MenuSecondTableViewCell
+    {
         guard let cellForReturn = tableView.dequeueReusableCell(withIdentifier: "secondTableViewCell",for: indexPath) as? MenuSecondTableViewCell
         else
         {
@@ -42,37 +50,38 @@ extension MenuViewController: UITableViewDelegate, UITableViewDataSource{
         cellForReturn.button.setTitle(title, for: .normal)
         cellForReturn.button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
         cellForReturn.button.tag = indexPath.row
+        
+        
+        var configuration = UIButton.Configuration.plain()
+        configuration.image =  UIImage(named: imagePath)!.resizedImage(Size: CGSize(width: 40, height: 40))!
+        configuration.titlePadding = 10
+        configuration.imagePadding = 10
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 10)
+       
+        cellForReturn.button.configuration = configuration
         return cellForReturn
     }
     
-   
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch indexPath.row{
-        case 0:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "firstTableViewCell",for: indexPath) as? MenuFirstTableViewCell
-            else
-            {
-                assertionFailure("Cannot dequeue reusable cell \(MenuFirstTableViewCell.self) with reuseIdentifier: tableViewCell")
-                return UITableViewCell()
-            }
-            cell.button.setTitle("INTERACTIVE", for: .normal)
-            return cell
-        case 1:
-            return guardTableView(title: "MAIN", indexPath: indexPath)
-        case 2:
-            return guardTableView(title: "RULES", indexPath: indexPath)
-        case 3:
-            return guardTableView(title: "DETAILS", indexPath: indexPath)
-        case 4:
-            return guardTableView(title: "INVENTORY", indexPath: indexPath)
-        case 5:
-            return guardTableView(title: "WORLD CHAMPIONSHIPS", indexPath: indexPath)
-        case 6:
-            return guardTableView(title: "FAMOUS ATHLETES", indexPath: indexPath)
-        default:
-            return guardTableView(title: "", indexPath: indexPath)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        switch indexPath.row
+        {
+            case 0:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "firstTableViewCell",for: indexPath) as? MenuFirstTableViewCell
+                else
+                {
+                    assertionFailure("Cannot dequeue reusable cell \(MenuFirstTableViewCell.self) with reuseIdentifier: tableViewCell")
+                    return UITableViewCell()
+                }
+                cell.button.setTitle("INTERACTIVE", for: .normal)
+                return cell
+            case 1: return initCell(title: "MAIN", indexPath: indexPath,imagePath: "main")
+            case 2: return initCell(title: "RULES", indexPath: indexPath,imagePath: "rules")
+            case 3: return initCell(title: "DETAILS", indexPath: indexPath, imagePath: "details")
+            case 4: return initCell(title: "INVENTORY", indexPath: indexPath, imagePath: "inventory")
+            case 5: return initCell(title: "WORLD CHAMPIONSHIPS", indexPath: indexPath, imagePath: "worldchamp")
+            case 6: return initCell(title: "FAMOUS ATHLETES", indexPath: indexPath, imagePath: "famous")
+            default: return initCell(title: "", indexPath: indexPath, imagePath: "")
         }
-       
     }
 }
