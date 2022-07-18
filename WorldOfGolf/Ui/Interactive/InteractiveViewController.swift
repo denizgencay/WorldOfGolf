607ab2e3e0a28e54b8ef37557c2857b868489ce0
Cem <cemsertkaya99@gmail.com>
2022-07-18 15:08:38 +0300

    Interactive page is completed except popUp constraints

--- WorldOfGolf/Ui/Interactive/InteractiveViewController.swift
+++ WorldOfGolf/Ui/Interactive/InteractiveViewController.swift
@@ -8,22 +8,45 @@
 import UIKit
 
 class InteractiveViewController: UIViewController {
+    @IBOutlet weak var ball1: UIImageView!
+    @IBOutlet weak var ball2: UIImageView!
+    @IBOutlet weak var ball3: UIImageView!
+    @IBOutlet weak var ball4: UIImageView!
+    @IBOutlet weak var ball5: UIImageView!
+    @IBOutlet var popUpView: UITextView!
+
     let uiGlobal = UIGlobal.shared
-    override func viewDidLoad() {
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         self.navigationController?.navigationBar.topItem?.backBarButtonItem = uiGlobal.createBackButtonItem(title: "Interactive")
-        // Do any additional setup after loading the view.
+        makeTouchableImage(image: ball1)
+        makeTouchableImage(image: ball2)
+        makeTouchableImage(image: ball3)
+        makeTouchableImage(image: ball4)
+        makeTouchableImage(image: ball5)
+        
+    }
+    
+    func makeTouchableImage(image : UIImageView)
+    {
+        let cameraTap = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
+        image.isUserInteractionEnabled = true
+        image.addGestureRecognizer(cameraTap)
+    }
+                                               
+    @objc func imageTapped()
+    {
+        popUpView.center = view.center
+       
+       
+        view.addSubview(popUpView)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
 
 }
