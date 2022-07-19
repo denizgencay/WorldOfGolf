9beb9677beeeb190da22a78accee115bc2e120d6
denizgencay <denizgencay35@gmail.com>
2022-07-19 16:42:12 +0300

    Expandable cell implemented

--- WorldOfGolf/Ui/Rules/RulesViewController.swift
+++ WorldOfGolf/Ui/Rules/RulesViewController.swift
@@ -7,23 +7,115 @@
 
 import UIKit
 
-class RulesViewController: UIViewController {
+
+class Section{
+    let title: String
+    let options: [String]
+    var isOpened: Bool = false
+    
+    init(
+        title: String,
+        options: [String],
+        isOpened: Bool = false
+    ){
+        self.title = title
+        self.options = options
+        self.isOpened = isOpened
+    }
+}
+
+class RulesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     let uiGlobal = UIGlobal.shared
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var sections = [Section]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.topItem?.backBarButtonItem = uiGlobal.createBackButtonItem(title: "Rules")
+        tableView.delegate = self
+        tableView.dataSource = self
+       // tableView.rowHeight = UITableView.automaticDimension
+       // tableView.estimatedRowHeight = 600
+    
+        
+        sections = [
+            Section(title: "Section 1", options: ["kahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfhkahsdkflahlskdjfhkasdhfakjsdfh"]),
+            Section(title: "Section 2", options: [1, 2, 3].compactMap({return "Cell \($0)"})),
+            Section(title: "Section 3", options: [1, 2, 3].compactMap({return "Cell \($0)"})),
+            Section(title: "Section 4", options: [1, 2, 3].compactMap({return "Cell \($0)"}))
+        ]
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = sections[section]
+        
+        if section.isOpened == true{
+            return section.options.count + 1
+        }else{
+            return 1
+        }
+        
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "expandableViewCell", for: indexPath)
+        cell.selectionStyle = .none
+        cell.textLabel?.numberOfLines = 0
+        if indexPath.row == 0{
+            cell.textLabel?.text = sections[indexPath.section].title
+        }else{
+            cell.textLabel?.text = sections[indexPath.section].options[indexPath.row - 1]
+        }
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        sections[indexPath.section].isOpened = !sections[indexPath.section].isOpened
+        tableView.reloadSections([indexPath.section], with: .middle)
+        
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
