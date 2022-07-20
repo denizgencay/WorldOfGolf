bdfa4d8f5f41b573f124a41803cd71e4646ba36b
Cem <cemsertkaya99@gmail.com>
2022-07-20 17:41:29 +0300

    UI updates

---
+++ WorldOfGolf/Ui/Rules/InsideTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InsideTableViewCell.swift
+//  WorldOfGolf
+//
+//  Created by Cem Sertkaya on 20.07.2022.
+//
+
+import UIKit
+
+class InsideTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
